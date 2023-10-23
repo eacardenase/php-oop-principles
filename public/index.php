d38b8dbe6957297bc2349a05f493d9a205de0fa4
Edwin Cardenas <eacardenase@unal.edu.co>
2023-10-22 19:37:55 -0500

    feat: composition

--- public/index.php
+++ public/index.php
@@ -4,4 +4,5 @@
 //require __DIR__ . '/../constructs/inheritance.php';
 //require __DIR__ . '/../constructs/abstract-classes.php';
 //require __DIR__ . '/../constructs/interfaces.php';
-require __DIR__ . '/../constructs/encapsulation.php';
+//require __DIR__ . '/../constructs/encapsulation.php';
+require __DIR__ . '/../constructs/composition.php';
